d3aaec5365e7467b999006b5851fc00b3cea021f
Erwan Demairy <erwan.demairy@inria.fr>
2015-12-11 15:56:36 +0100

    Addition of the first example

---
+++ SimpleMain/src/main/java/fr/inria/coreseTutorial/App.java
@@ -0,0 +1,28 @@
+package fr.inria.coreseTutorial;
+import fr.inria.acacia.corese.exceptions.EngineException;
+import fr.inria.edelweiss.kgram.core.Mappings;
+import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.query.QueryProcess;
+import fr.inria.edelweiss.kgtool.load.Load;
+import fr.inria.edelweiss.kgtool.print.ResultFormat;
+import fr.inria.edelweiss.kgtool.print.TripleFormat;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) throws EngineException
+    {
+                Graph graph = Graph.create();
+                Load ld = Load.create(graph);
+                ld.load("human1.rdf");
+                QueryProcess exec = QueryProcess.create(graph);
+                String query = "select * where {?x ?p ?y}";
+                Mappings map = exec.query(query);
+                ResultFormat f1 = ResultFormat.create(map);
+                System.out.println(f1);
+                TripleFormat f2 = TripleFormat.create(graph);
+                System.out.println(f2);
+    }
+}
