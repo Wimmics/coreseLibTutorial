296cc25de9f51da2e1e432f08acdeb800afd4280
Erwan Demairy <erwan.demairy@inria.fr>
2016-02-23 14:36:07 +0100

    Example of interactions with the REST SPARQL API of Corese.

--- server/src/main/java/fr/inria/wimmics/corese/tutorial/server/CoreseClient.java
+++ server/src/main/java/fr/inria/wimmics/corese/tutorial/server/CoreseClient.java
@@ -6,17 +6,31 @@
 package fr.inria.wimmics.corese.tutorial.server;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import javax.ws.rs.core.MultivaluedMap;
 
 /**
  *
  * @author edemairy
  */
-public class Request {
+public class CoreseClient {
+
+	static public int PORT = 8080;
+
+	public static void reset() throws URISyntaxException {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource service = client.resource(new URI("http://localhost:" + PORT + "/"));
+		System.out.println(service.path("sparql").path("reset").post(String.class).toString());
+	}
+
 	public static void query() throws URISyntaxException, MalformedURLException, IOException {
 
 		String query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
@@ -28,7 +42,7 @@ public class Request {
 
 		ClientConfig config = new DefaultClientConfig();
 		Client client = Client.create(config);
-		WebResource service = client.resource(new URI("http://localhost:" + RestEndpointTest.port + "/kgram"));
+		WebResource service = client.resource(new URI("http://localhost:" + PORT + "/kgram"));
 
 		System.out.println(service.path("sparql").path("reset").post(String.class).toString());
 
@@ -38,5 +52,31 @@ public class Request {
 		service.path("sparql").path("load").post(formData);
 		System.out.println(service.path("sparql").queryParam("query", query).accept("application/sparql-results+xml").get(String.class));
 //        System.out.println(service.path("sparql").queryParam("query", query).accept("application/json").get(String.class));
-	}	
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		String menu = "1. reset\n"
+			+ "2. load\n"
+			+ "3. query\n"
+			+ "4. exit\n";
+		int value = -1;
+		do {
+			do {
+				System.out.println(menu);
+				System.out.print("Your choice: ");
+				value = System.in.read();
+			} while (value < Character.valueOf('1') && value > Character.valueOf('4'));
+			value = value - Character.valueOf('1') + 1;
+				System.out.println("value read = " + value);
+			switch (value) {
+				case 1:
+					reset();
+					break;
+//				case 2: load(); break;
+				case 3:
+					query();
+					break;
+			}
+		} while (value != 4);
+	}
 }
